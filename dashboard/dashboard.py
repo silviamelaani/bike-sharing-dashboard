9b41c1666c3c854e81b34d7c5eb11d02932c61ea
silviamelaani <silviamelani90@gmail.com>
2026-04-22 02:19:22 +0700

    gila dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -4,10 +4,10 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import os
 
-absolute_path = os.path.dirname(__file__)
+basepath = os.path.dirname(__file__)
 
-day_df = pd.read_csv(os.path.join(absolute_path, "day.csv"))
-hour_df = pd.read_csv(os.path.join(absolute_path, "hour.csv"))
+day_df = pd.read_csv(os.path.join(basepath, "day.csv"))
+hour_df = pd.read_csv(os.path.join(basepath, "hour.csv"))
 
 st.set_page_config(
     page_title="Bike Sharing Dashboard",
